755c50f1c02db4dfb961648d5cde706da273a2ff
rishabh-raj-tech <rishabhsinghrajput085@gmail.com>
2026-08-15 22:10:56 +0530

    Time: 3 ms (60.98%) | Memory: 19.3 MB (76.06%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freq = {}
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+
+        nums.sort(key=lambda x: (freq[x], -x))
+
+        return nums
