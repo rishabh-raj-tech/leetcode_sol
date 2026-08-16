712e3c810cd355aff3cc04bc9ee9e5c2b635bc4b
rishabh-raj-tech <rishabhsinghrajput085@gmail.com>
2026-08-16 16:32:50 +0530

    Time: 2 ms (52.58%) | Memory: 19.3 MB (14.45%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def alternatingSum(self, nums: List[int]) -> int:
+        even = 0
+        odd = 0
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                even += nums[i]
+            else:
+                odd += nums[i]
+        return even - odd
